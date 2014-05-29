3c895587d063e0e063a94af97176ac2cfcf0a0b8
Mondain <mondain@gmail.com>
2014-05-29 13:18:29 -0700

    Fixed to comply with RFC6455

--- src/main/java/org/red5/net/websocket/WebSocketHandshake.java
+++ src/main/java/org/red5/net/websocket/WebSocketHandshake.java
@@ -4,6 +4,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import org.apache.mina.core.buffer.IoBuffer;
+import org.bouncycastle.util.encoders.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,18 +13,25 @@ import org.red5.server.plugin.PluginRegistry;
 /**
  * WebSocketHandshake
  * <pre>
- * this class for handshake process.
+ * This class handles the handshake process. WebSocket version 13 is required.
  * </pre>
+ * @see <a href="https://developer.mozilla.org/en-US/docs/WebSockets/Writing_WebSocket_servers">Mozilla - Writing WebSocket Servers</a>
+ *  
+ * @author Paul Gregoire
  */
 public class WebSocketHandshake {
 
 	private static final Logger log = LoggerFactory.getLogger(WebSocketHandshake.class);
+
+	// concatenate this with the clients key
+	private static final String WEBSOCKET_MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+	
+	private static final byte[] CRLF = { 0x0D, 0x0A };
 	
 	private WebSocketConnection conn;
 
-	private String key1;
-
-	private String key2;
+	// clients key
+	private String key;
 
 	private String origin;
 
@@ -31,8 +39,10 @@ public class WebSocketHandshake {
 
 	private String path;
 
-	private byte[] key3;
-
+	WebSocketHandshake() {
+		
+	}
+	
 	/**
 	 * constructor with connection object.
 	 * @param conn connection object.
@@ -66,21 +76,23 @@ public class WebSocketHandshake {
 						WebSocketScopeManager manager = ((WebSocketPlugin) PluginRegistry.getPlugin("WebSocketPlugin")).getManager();
 						if (ary.length <= 1 || !manager.isEnabled(ary[1])) {
 							// scope is not application. handshake will be false;
-							// send disconnect message.
+							// send disconnect message
 							IoBuffer buf = IoBuffer.allocate(4);
 							buf.put(new byte[] { (byte) 0xFF, (byte) 0x00 });
 							buf.flip();
 							conn.send(buf);
-							// close connection.
+							// close connection
 							conn.close();
 							throw new WebSocketException("Handshaking failed");
 						}
-					} else if (data.contains("Sec-WebSocket-Key1")) {
-						// get the key1 data
-						key1 = data;
-					} else if (data.contains("Sec-WebSocket-Key2")) {
-						// get the key2 data
-						key2 = data;
+					} else if (data.contains("Sec-WebSocket-Key")) {
+						// get the key data
+						key = data.substring(data.indexOf(':') + 2);
+					} else if (data.contains("Sec-WebSocket-Version")) {
+						// check the version
+						if (data.indexOf("13") == -1) {
+							log.info("Version 13 was not found in the request, handshaking may fail");
+						}
 					} else if (data.contains("Host")) {
 						// get the host data
 						String[] ary = data.split("Host: ");
@@ -92,7 +104,7 @@ public class WebSocketHandshake {
 						origin = ary[1];
 						conn.setOrigin(ary[1]);
 					}
-					// for the information print out the string data.
+					// for the information print out the string data
 					if (data.length() > 4) {
 						log.debug(data);
 					}
@@ -104,7 +116,6 @@ public class WebSocketHandshake {
 				i++;
 			}
 		}
-		key3 = b;
 		// start the handshake reply
 		doHandShake();
 	}
@@ -114,58 +125,32 @@ public class WebSocketHandshake {
 	 * @param key3
 	 */
 	private void doHandShake() throws WebSocketException {
-		if (key3 == null) {
-			throw new WebSocketException("last byte is incorrect!");
+		if (key == null) {
+			throw new WebSocketException("Key data is missing");
 		}
-		if (key1 == null || key2 == null) {
-			throw new WebSocketException("key data is missing");
-		}
-		// calicurate first 16 byte of integer data;
-		byte[] b = new byte[16];
-		int buf1 = getKeyInteger(key1);
-		int buf2 = getKeyInteger(key2);
-		byte[] result;
+		byte[] accept;
 		try {
-			b[0] = (byte) ((buf1 & 0xFF000000) >> 24);
-			b[1] = (byte) ((buf1 & 0x00FF0000) >> 16);
-			b[2] = (byte) ((buf1 & 0x0000FF00) >> 8);
-			b[3] = (byte) ((buf1 & 0x000000FF));
-			b[4] = (byte) ((buf2 & 0xFF000000) >> 24);
-			b[5] = (byte) ((buf2 & 0x00FF0000) >> 16);
-			b[6] = (byte) ((buf2 & 0x0000FF00) >> 8);
-			b[7] = (byte) ((buf2 & 0x000000FF));
-			b[8] = key3[0];
-			b[9] = key3[1];
-			b[10] = key3[2];
-			b[11] = key3[3];
-			b[12] = key3[4];
-			b[13] = key3[5];
-			b[14] = key3[6];
-			b[15] = key3[7];
-			// make MD5 byte data.
-			result = crypt(b);
+			// concatenate the key and magic string, then SHA1 hash and base64 encode
+			accept = crypt(key + WEBSOCKET_MAGIC_STRING);
 		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			throw new WebSocketException("MD5 algorithm is missing");
-		} catch (ArrayIndexOutOfBoundsException e) {
-			e.printStackTrace();
-			throw new WebSocketException("Data is too short");
+			throw new WebSocketException("Algorithm is missing");
 		}
 		// make up reply data...
 		IoBuffer buf = IoBuffer.allocate(2048);
-		byte[] bb = { 0x0D, 0x0A };
-		buf.put("HTTP/1.1 101 WebSocket Protocol Handshake".getBytes());
-		buf.put(bb);
-		buf.put("Upgrade: WebSocket".getBytes());
-		buf.put(bb);
+		buf.put("HTTP/1.1 101 Switching Protocols".getBytes());
+		buf.put(CRLF);
+		buf.put("Upgrade: websocket".getBytes());
+		buf.put(CRLF);
+		buf.put("Connection: Upgrade".getBytes());
+		buf.put(CRLF);
 		buf.put(("Sec-WebSocket-Origin: " + origin).getBytes());
-		buf.put(bb);
-		buf.put(("Sec-WebSocket-Location: ws://" + host + path).getBytes());
-		buf.put(bb);
-		buf.put("Sec-WebSocket-Protocol: sample".getBytes());
-		buf.put(bb);
-		buf.put(bb);
-		buf.put(result);
+		buf.put(CRLF);
+		buf.put(String.format("Sec-WebSocket-Location: ws://%s%s", host, path).getBytes());
+		buf.put(CRLF);
+		buf.put(String.format("Sec-WebSocket-Accept: %s", new String(accept)).getBytes());
+		buf.put(CRLF);
+		buf.put(CRLF);
+		buf.put(accept);
 		buf.flip();
 		// write the data on session
 		conn.getSession().write(buf);
@@ -177,37 +162,30 @@ public class WebSocketHandshake {
 	}
 
 	/**
-	 * calicurate integer data.
-	 * @param key input key string data.
-	 * @return integer data after calicurate
-	 */
-	private Integer getKeyInteger(String key) {
-		StringBuffer numList = new StringBuffer();
-		int spaceCount = 0;
-		for (int i = 20; i < key.length(); i++) {
-			char c = key.charAt(i);
-			if (c >= 0x30 && c < 0x3A) {
-				// this is number data.
-				numList.append(c);
-			} else if (c == ' ') {
-				// this is space data.
-				spaceCount++;
-			}
-		}
-		return (int) (new Long(numList.toString()) / spaceCount);
-	}
-
-	/**
-	 * make md5 data.
-	 * @param bytes input bytes
-	 * @return crypted bytes data
+	 * Perform the accept creation routine from RFC6455.
+	 * @see <a href="http://tools.ietf.org/html/rfc6455">RFC6455</a>
+	 * 
+	 * @param in input data
+	 * @return crypted data
 	 * @throws NoSuchAlgorithmException
 	 */
-	private byte[] crypt(byte[] bytes) throws NoSuchAlgorithmException {
-		if (bytes == null || bytes.length == 0) {
-			throw new IllegalArgumentException("bytes for encrypt must have body");
+	public byte[] crypt(String in) throws NoSuchAlgorithmException {
+		log.debug("Crypt: >{}<", in);
+		if (in == null || in.length() == 0) {
+			throw new IllegalArgumentException("Missing body for accept calculation");
 		}
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		return md.digest(bytes);
+		MessageDigest md = MessageDigest.getInstance("SHA1");
+		byte[] crypted = Base64.encode(md.digest(in.getBytes()));
+		return crypted;
 	}
+	
+	public static void main(String[] args) throws Exception {
+		WebSocketHandshake hs = new WebSocketHandshake();
+		if ("s3pPLMBiTxaQ9kYGzzhZRbK+xOo=".equals(new String(hs.crypt("dGhlIHNhbXBsZSBub25jZQ==258EAFA5-E914-47DA-95CA-C5AB0DC85B11")))) {
+			System.out.println("Accept routine is valid");
+		} else {
+			System.err.println("Accept routine is invalid!");
+		}
+	}
+	
 }
