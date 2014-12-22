4686011cf963575112171b5488bd0f5f73e191a2
Paul Gregoire <mondain@gmail.com>
2014-12-22 14:17:17 -0500

    Updated pom

--- src/test/java/org/red5/net/websocket/WebSocketServerTest.java
+++ src/test/java/org/red5/net/websocket/WebSocketServerTest.java
@@ -32,6 +32,9 @@ import java.util.Set;
 //import net.sourceforge.groboutils.junit.v1.MultiThreadedTestRunner;
 //import net.sourceforge.groboutils.junit.v1.TestRunnable;
 
+
+import net.sourceforge.groboutils.junit.v1.TestRunnable;
+
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.filterchain.IoFilter.NextFilter;
 import org.apache.mina.core.filterchain.IoFilterChain;
