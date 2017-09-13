faaf57e7685b2c5496bb259847946781d8df04c3
Paul Gregoire <mondain@gmail.com>
2017-09-13 11:37:29 -0700

    Refactored ssl/tls in an attempt to fix ff 55

--- src/main/java/org/red5/net/websocket/SecureWebSocketConfiguration.java
+++ src/main/java/org/red5/net/websocket/SecureWebSocketConfiguration.java
@@ -22,16 +22,26 @@ import java.io.File;
 import java.io.NotActiveException;
 import java.security.KeyStore;
 import java.security.Provider;
+import java.security.Provider.Service;
 import java.security.Security;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
+import javax.net.ssl.SNIHostName;
+import javax.net.ssl.SNIMatcher;
+import javax.net.ssl.SNIServerName;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLParameters;
 
 import org.apache.mina.filter.ssl.KeyStoreFactory;
 import org.apache.mina.filter.ssl.SslContextFactory;
 import org.apache.mina.filter.ssl.SslFilter;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -76,7 +86,11 @@ public class SecureWebSocketConfiguration {
 
     static {
         // add bouncycastle security provider
-        Security.addProvider(new BouncyCastleProvider());
+        int insertedAt = Security.insertProviderAt(new BouncyCastleProvider(), 1);
+        log.debug("BC provider inserted at position: {}", insertedAt);
+        //insertedAt = Security.insertProviderAt(new BouncyCastleJsseProvider(), 2);
+        //log.debug("BC JSSE provider inserted at position: {}", insertedAt);
+        //Security.addProvider(new BouncyCastleProvider());
         if (log.isTraceEnabled()) {
             Provider[] providers = Security.getProviders();
             for (Provider provider : providers) {
@@ -118,6 +132,29 @@ public class SecureWebSocketConfiguration {
             File trustStore = new File(truststoreFile);
             log.trace("Truststore - read: {} path: {}", trustStore.canRead(), trustStore.getCanonicalPath());
             if (keyStore.exists() && trustStore.exists()) {
+                // ssl context factory
+                final SslContextFactory sslContextFactory = new SslContextFactory();
+                // enforce TLSv1.2 otherwise we may get a lesser protocol
+                sslContextFactory.setProtocol("TLSv1.2");
+                // get provider
+                Provider prov = Security.getProvider(BouncyCastleJsseProvider.PROVIDER_NAME);
+                if (prov != null) {
+                    if (log.isDebugEnabled()) {
+                        for (String prop : prov.stringPropertyNames()) {
+                            log.debug("Property name: {}", prop);
+                        }
+                        Set<Service> svcs = prov.getServices();
+                        for (Service svc : svcs) {
+                            log.debug("Service - type: {} class: {}", svc.getType(), svc.getClassName());
+                        }
+                    }
+                    sslContextFactory.setProvider(BouncyCastleJsseProvider.PROVIDER_NAME);
+                    //keyStoreFactory.setProvider(BouncyCastleJsseProvider.PROVIDER_NAME);
+                    //trustStoreFactory.setProvider(BouncyCastleJsseProvider.PROVIDER_NAME);
+                } else {
+                    // use whatever default is available
+                    log.debug("BouncyCastleJsseProvider not found");
+                }
                 // keystore
                 final KeyStoreFactory keyStoreFactory = new KeyStoreFactory();
                 keyStoreFactory.setDataFile(keyStore);
@@ -126,9 +163,6 @@ public class SecureWebSocketConfiguration {
                 final KeyStoreFactory trustStoreFactory = new KeyStoreFactory();
                 trustStoreFactory.setDataFile(trustStore);
                 trustStoreFactory.setPassword(truststorePassword);
-                // ssl context factory
-                final SslContextFactory sslContextFactory = new SslContextFactory();
-                //sslContextFactory.setProtocol("TLS");
                 // get keystore
                 final KeyStore ks = keyStoreFactory.newInstance();
                 sslContextFactory.setKeyManagerFactoryKeyStore(ks);
@@ -139,17 +173,53 @@ public class SecureWebSocketConfiguration {
                 // get ssl context
                 sslContext = sslContextFactory.newInstance();
                 log.debug("SSL provider: {}", sslContext.getProvider());
+                // SNI state
+                boolean sniEnabled = Boolean.valueOf(System.getProperty("jsse.enableSNIExtension", "false"));
                 // get ssl context parameters
                 SSLParameters params = sslContext.getDefaultSSLParameters();
                 if (log.isDebugEnabled()) {
                     log.debug("SSL context params - need client auth: {} want client auth: {} endpoint id algorithm: {}", params.getNeedClientAuth(), params.getWantClientAuth(), params.getEndpointIdentificationAlgorithm());
                     String[] supportedProtocols = params.getProtocols();
-                    for (String protocol : supportedProtocols) {
-                        log.debug("SSL context supported protocol: {}", protocol);
+                    if (supportedProtocols != null) {
+                        for (String protocol : supportedProtocols) {
+                            log.debug("SSL context supported protocol: {}", protocol);
+                        }
+                    } else {
+                        log.debug("No protocols");
+                    }
+                    String[] supportedCiphers = params.getCipherSuites();
+                    if (supportedCiphers != null) {
+                        for (String cipher : supportedCiphers) {
+                            log.debug("SSL context supported cipher: {}", cipher);
+                        }
+                    } else {
+                        log.debug("No ciphers");
+                    }
+                    // http://docs.oracle.com/javase/8/docs/technotes/guides/security/jsse/JSSERefGuide.html#SNIExamples
+                    log.debug("SNI extension enabled: {}", sniEnabled);
+                    List<SNIServerName> serverNames = params.getServerNames();
+                    if (serverNames != null) {
+                        for (SNIServerName sname : serverNames) {
+                            log.debug("SNI server name: {}", sname);
+                        }
+                    } else {
+                        log.debug("No SNI server names specified");
+                    }
+                    Collection<SNIMatcher> sniMatchers = params.getSNIMatchers();
+                    if (sniMatchers != null) {
+                        for (SNIMatcher sniMatcher : sniMatchers) {
+                            log.debug("SNI matcher: {}", sniMatcher);
+                        }
+                    } else {
+                        log.debug("No SNI matchers specified");
                     }
                 }
-                // compatibility: remove the SSLv2Hello message in the available protocols - some systems will fail 
-                // to handshake if TSLv1 messages are enwrapped with SSLv2 messages, Java 6 tries to send TSLv1 embedded in SSLv2
+                if (sniEnabled) {
+                    SNIMatcher matcher = SNIHostName.createSNIMatcher("");
+                    Collection<SNIMatcher> matchers = new ArrayList<>(1);
+                    matchers.add(matcher);
+                    params.setSNIMatchers(matchers);
+                }
             } else {
                 log.warn("Keystore or Truststore file does not exist");
             }
